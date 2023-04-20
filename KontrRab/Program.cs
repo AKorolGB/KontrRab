cfd78934b65951a7ebd5702ba76bbb63f106883c
AKorolGB <Alexander94.13@mail.ru>
2023-04-20 17:36:57 +0200

    Решение задачи

---
+++ KontrRab/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"Введите {i+1} элемент массива: ");
+  array[i]= Console.ReadLine();
+}
+int longb = 0;
+int f = 0;
+for (int i = 0; i < array.Length; i++)
+if (array[i].Length <= 3)
+{
+longb = longb +1;
+}
+
+string[] barray = new string[longb];    
+for (int i = 0; i < array.Length; i++)
+if (array[i].Length <= 3){
+barray[f] = array[i];
+f = f +1;
+}
+
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Новый  массив: [{string.Join(", ", barray)}]");
+
